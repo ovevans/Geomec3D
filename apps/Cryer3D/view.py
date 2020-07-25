9423e7143dee2489f015e9bdfa5ed69fb2b86661
casfeq <casf.eq@gmail.com>
2020-07-25 20:48:55 -0300

    Added and verified mixed formulation

--- apps/Cryer3D/view.py
+++ apps/Cryer3D/view.py
@@ -34,20 +34,25 @@ for key in properties:
 properties = properties[key]
 reader1 = HDF5Reader(directory1, resultsFile)
 reader2 = HDF5Reader(directory2, resultsFile)
+reader3 = HDF5Reader(directory3, resultsFile)
 # Exact solution
 cryer = Cryer(properties, settings, numOfRoots=100)
 # Arrange data
 t1 = []
 t2 = []
+t3 = []
 p1 = []
 p2 = []
+p3 = []
 step = 0
 numOfSteps = reader1.getNumOfSteps()
 while step < numOfSteps:
 	t1.append(float(reader1.getTimeLevel(step))/3.6e3)
 	t2.append(float(reader2.getTimeLevel(step))/3.6e3)
+	t3.append(float(reader3.getTimeLevel(step))/3.6e3)
 	p1.append(float(reader1.getSolutionAtNodeAndStep("p", reader1.getNode(Point(0., 0., 0.)), step))/1e3)
 	p2.append(float(reader2.getSolutionAtNodeAndStep("p", reader2.getNode(Point(0., 0., 0.)), step))/1e3)
+	p3.append(float(reader3.getSolutionAtNodeAndStep("p", reader3.getNode(Point(0., 0., 0.)), step))/1e3)
 	step += 6
 tmin = min(t1)
 tmax = max(t1)
@@ -57,6 +62,7 @@ fig = plt.figure(figsize=(8, 5))
 plt.plot(tExact, [cryer.exactSolution(t*3.6e3)/1e3 for t in tExact], "-", color="grey", label="Exact")
 plt.plot(t1, p1, mark1, fillstyle="none", label=label1, ms=6, mec="k", mew=0.75)
 plt.plot(t2, p2, mark2, fillstyle="none", label=label2, ms=6, mec="k", mew=0.75)
+plt.plot(t3, p3, mark3, fillstyle="none", label=label3, ms=6, mec="k", mew=0.75)
 plt.xlabel('Time (hours)')
 plt.ylabel('Pressure (kPa)')
 plt.grid(which='major', axis='both')
--- libs/spaces.py
+++ libs/spaces.py
@@ -18,3 +18,23 @@ class DisplacementPressureSpace(object):
 
 	def function(self):
 		return BlockFunction(self.mixedSpace)
+
+
+class DisplacementPressureVolStressSpace(object):
+	def __init__(self, grid, u_degree, p_degree, sig_degree):
+		v_elem = VectorElement("CG", grid.mesh.ufl_cell(), u_degree)
+		q_elem = FiniteElement("CG", grid.mesh.ufl_cell(), p_degree)
+		tau_elem = FiniteElement("CG", grid.mesh.ufl_cell(), sig_degree)
+		self.V = FunctionSpace(grid.mesh, v_elem)
+		self.Q = FunctionSpace(grid.mesh, q_elem)
+		self.T = FunctionSpace(grid.mesh, tau_elem)
+		self.mixedSpace = BlockFunctionSpace([self.V, self.Q, self.T])
+
+	def trialFunction(self):
+		return block_split(BlockTrialFunction(self.mixedSpace))
+
+	def testFunction(self):
+		return block_split(BlockTestFunction(self.mixedSpace))
+
+	def function(self):
+		return BlockFunction(self.mixedSpace)
